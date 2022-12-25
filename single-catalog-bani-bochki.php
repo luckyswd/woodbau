144639f17681859185ab33b90ee3285f4126eec5
Slava Liutkevich <slava.lyutkevich8308@gmail.com>
2022-12-26 00:32:14 +0300

    Create template for single product with category 'bani-bochki'

---
+++ single-catalog-bani-bochki.php
@@ -0,0 +1,236 @@
+<?php
+
+$images = get_field('main_gallery');
+$properties = get_field('properties');
+$price = get_field('price');
+$services = get_field('services');
+
+// Вкладки под товаром
+// Планировка
+$planningInfo = get_field('planning_info');
+$planningImage = get_field('planning_image');
+
+// Двери
+$doorsInfo = get_field('doors_info');
+
+// Фасады
+$facadesInfo = get_field('facades_info');
+
+// Фото
+$photos = get_field('photos');
+
+// Комплект
+$setFirstTitle = get_field('set_first_title');
+$setList = get_field('set_list');
+$setSecondTitle = get_field('set_second_title');
+$setDropDownList = get_field('set_drop-down_list');
+
+// Дополнительно
+$additionalTitle = get_field('additional_title');
+$additionalContent = get_field('additional_content');
+
+?>
+
+<section class="slider-info">
+    <div class="container">
+        <div class="slider-info__wrapper">
+            <h1 class="slider-mobile-title"><?= the_title() ?></h1>
+            <div class="slider-info__left">
+                <div class="swiper mySwiper2">
+                    <div class="swiper-wrapper">
+                        <?php if (!empty($images)) : ?>
+                            <?php foreach ($images as $image) {
+                                  $image = $image['url'];
+                                ?>
+                                <div class="swiper-slide">
+                                    <a href="javascript:;" data-fancybox="slider" data-src="<?= $image ?>"
+                                       class="slider-open-window">
+                                        <picture>
+                                            <img class="slide-main-image" src="<?= $image; ?>" alt="slide">
+                                        </picture>
+                                    </a>
+                                </div>
+                            <?php } ?>
+                        <?php endif; ?>
+                    </div>
+                    <div class="swiper-pagination"></div>
+                </div>
+            </div>
+            <div class="slider-info__right">
+                <h1><?= the_title() ?></h1>
+                    <?php if (!empty($properties)) : ?>
+                        <div class="properties">
+                            <div class="properties-title">Характеристики</div>
+                            <div class="properties-items">
+                                <?php foreach ($properties as $property) : ?>
+                                    <?php if ($property['property_name'] && $property['property_value']) : ?>
+                                        <div class="property-item">
+                                            <div class="property-item-name"><?= $property['property_name'] ?></div>
+                                            <div class="property-item-value"><?= $property['property_value'] ?></div>
+                                        </div>
+                                    <?php endif; ?>
+                                <?php endforeach; ?>
+                            </div>
+                        </div>
+                    <?php endif; ?>
+                    <?php if (!empty($price)) : ?>
+                        <div class="slider-info__right-price">Цена: <span><?= $price ?></span> руб.</div>
+                    <?php endif; ?>
+                    <?php if (!empty($services)) : ?>
+                        <div class="slider-info__services">
+                            <h4>Возможные услуги</h4>
+                            <?php foreach ($services as $service) : ?>
+                                <?php if ($service['service_name'] && $service['service_price']) : ?>
+                                    <div class="service-item">
+                                        <div class="service-item-name"><?= $service['service_name'] ?></div>
+                                        <div class="service-item-switcher"
+                                             data-price="<?= $service['service_price'] ?>"><span></span></div>
+                                    </div>
+                                <?php endif; ?>
+                            <?php endforeach; ?>
+                        </div>
+                    <?php endif; ?>
+            </div>
+        </div>
+        <div class="slider-info-tabs">
+            <?php if (!empty($planningInfo)) : ?>
+                <h4 class="slider-info-tab-planning js-active-item" data-class="slider-info-planning">Планировка</h4>
+            <?php endif; ?>
+            <?php if (!empty($doorsInfo)) : ?>
+                <h4 class="slider-info-tab-doors" data-class="slider-info-doors">Двери</h4>
+            <?php endif; ?>
+            <?php if (!empty($facadesInfo)) : ?>
+                <h4 class="slider-info-tab-facades" data-class="slider-info-facades">Фасады</h4>
+            <?php endif; ?>
+            <?php if (!empty($photos)) : ?>
+                <h4 class="slider-info-tab-photos" data-class="slider-info-photos">Фотографии</h4>
+            <?php endif; ?>
+            <?php if (!empty($setFirstTitle)) : ?>
+                <h4 class="slider-info-tab-set" data-class="slider-info-set">Комплект</h4>
+            <?php endif; ?>
+            <?php if (!empty($additionalTitle)) : ?>
+                <h4 class="slider-info-tab-additional" data-class="slider-info-additional">Дополнительно</h4>
+            <?php endif; ?>
+        </div>
+        <div class="slider-info-content">
+            <?php if (!empty($planningInfo)) : ?>
+                <div class="slider-info-planning js-active-item">
+                    <?php foreach ($planningInfo as $planningInfoItem) : ?>
+                        <div class="planning-item">
+                            <div class="description-planning-name"><?= $planningInfoItem['text'] ?></div>
+                            <a href="<?= $planningInfoItem['file'] ?>" target="_blank" class="description-planning-value">Посмотреть все характеристики</a>
+                        </div>
+                    <?php endforeach; ?>
+                    <?php if (!empty($planningImage)) : ?>
+                      <img src="<?= $planningImage ?>" alt="Планировка">
+                    <?php endif; ?>
+                </div>
+            <?php endif; ?>
+            <?php if (!empty($doorsInfo)) : ?>
+                <div class="slider-info-doors">
+                    <?php foreach ($doorsInfo as $doorsInfoItem) : ?>
+                        <div class="doors-item">
+                            <?php if (!empty($doorsInfoItem['title'])) : ?>
+                                <div class="door-item-title tab-inner-title"><?= $doorsInfoItem['title'] ?></div>
+                            <?php endif; ?>
+                            <?php if (!empty($doorsInfoItem['text'])) : ?>
+                                <div class="door-item-text"><?= $doorsInfoItem['text'] ?></div>
+                            <?php endif; ?>
+                            <?php if (!empty($doorsInfoItem['image'])) : ?>
+                                <div class="door-item-image">
+                                    <img src="<?= $doorsInfoItem['image']['url'] ?>" alt="<?= $doorsInfoItem['image']['alt'] ?>" />
+                                </div>
+                            <?php endif; ?>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+            <?php endif; ?>
+            <?php if (!empty($facadesInfo)) : ?>
+                <div class="slider-info-facades">
+                    <?php foreach ($facadesInfo as $index => $facadesInfoItem) : ?>
+                        <div class="facades-item">
+                            <?php if (!empty($facadesInfoItem['title'])) : ?>
+                                <div class="facades-item-title tab-inner-title"><?= $facadesInfoItem['title'] ?></div>
+                            <?php endif; ?>
+                            <?php if (!empty($facadesInfoItem['text'])) : ?>
+                                <div class="facades-item-text"><?= $facadesInfoItem['text'] ?></div>
+                            <?php endif; ?>
+                            <?php if (!empty($facadesInfoItem['gallery'])) : ?>
+                                <div class="facades-item-gallery">
+                                    <?php foreach ($facadesInfoItem['gallery'] as $facadeImage) : ?>
+                                      <a href="javascript:;" data-fancybox="facades-<?= $index ?>" data-src="<?= $facadeImage['url'] ?>" class="slider-open-window">
+                                          <picture>
+                                              <img class="" src="<?= $facadeImage['url'] ?>" alt="<?= $facadeImage['alt'] ?>">
+                                          </picture>
+                                      </a>
+                                    <?php endforeach; ?>
+                                </div>
+                            <?php endif; ?>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+            <?php endif; ?>
+            <?php if (!empty($photos)) : ?>
+                <div class="slider-info-photos">
+                    <div class="photos-item">
+                        <div class="photos-item-gallery">
+                            <?php foreach ($photos as $photosImage) : ?>
+                              <a href="javascript:;" data-fancybox="photos" data-src="<?= $photosImage['url'] ?>" class="slider-open-window">
+                                  <picture>
+                                      <img class="" src="<?= $photosImage['url'] ?>" alt="<?= $photosImage['alt'] ?>">
+                                  </picture>
+                              </a>
+                            <?php endforeach; ?>
+                        </div>
+                    </div>
+                </div>
+            <?php endif; ?>
+            <?php if (!empty($setFirstTitle)) : ?>
+                <div class="slider-info-set">
+                    <div class="set-item">
+                        <div class="set-item-title tab-inner-title"><?= $setFirstTitle ?></div>
+                        <?php if (!empty($setList)) : ?>
+                            <ul class="set-item-list">
+                                <?php foreach ($setList as $setListItem) : ?>
+                                    <li><?= $setListItem['item'] ?></li>
+                                <?php endforeach; ?>
+                            </ul>
+                        <?php endif; ?>
+                        <?php if (!empty($setSecondTitle)) : ?>
+                            <div class="set-item-title tab-inner-title"><?= $setSecondTitle ?></div>
+                        <?php endif; ?>
+                        <?php if (!empty($setDropDownList)) : ?>
+                            <div class="set-item-dropdown-list">
+                                <?php foreach ($setDropDownList as $setDropDownListItem) : ?>
+                                    <div class="dropdown-list-item">
+                                        <?php if (!empty($setDropDownListItem['title'])) : ?>
+                                            <div class="dropdown-list-item-title"><?= $setDropDownListItem['title'] ?></div>
+                                        <?php endif; ?>
+                                        <?php if (!empty($setDropDownListItem['content'])) : ?>
+                                            <div class="dropdown-list-item-content"><?= $setDropDownListItem['content'] ?></div>
+                                        <?php endif; ?>
+                                        <?php if (!empty($setDropDownListItem['image'])) : ?>
+                                            <div class="dropdown-list-item-image">
+                                                <img src="<?= $setDropDownListItem['image']['url'] ?>" alt="<?= $setDropDownListItem['image']['alt'] ?>">
+                                            </div>
+                                        <?php endif; ?>
+                                    </div>
+                                <?php endforeach; ?>
+                            </div>
+                        <?php endif; ?>
+                    </div>
+                </div>
+            <?php endif; ?>
+            <?php if (!empty($additionalTitle)) : ?>
+                <div class="slider-info-additional">
+                    <div class="additional-item">
+                        <div class="additional-item-title tab-inner-title"><?= $additionalTitle ?></div>
+                        <?php if (!empty($additionalContent)) : ?>
+                            <div class="additional-item-content"><?= $additionalContent ?></div>
+                        <?php endif; ?>
+                    </div>
+                </div>
+            <?php endif; ?>
+        </div>
+    </div>
+</section>
--- single-catalog.php
+++ single-catalog.php
@@ -1,22 +1,38 @@
 <?php
 get_header();
+
+$post = $wp_query->post;
+$category = get_the_terms($post->ID, 'categories')[0]->slug;
+
+$customCategories = ['bani-bochki'];
+
+if ( in_array($category, $customCategories) ) { //ID категории
+    include( TEMPLATEPATH.'/single-catalog-'. $category .'.php' );
+} else {
+    include( TEMPLATEPATH.'/single-catalog-default.php' );
+}
+?>
+
+
+<?php
 // получение массива дополнительных полей
 $values = get_post_meta(get_the_ID(), false);
 $term_model = get_the_terms(get_the_ID(), 'categories');
 $term_model_item = $term_model["0"]->slug;
+$catId = $term_model[0]->term_taxonomy_id;
 
 $args = [
-    'post_type' => 'calculator',
+  'post_type' => 'calculator',
 ];
 
 $calculator = get_posts($args);
 $id;
 if (ICL_LANGUAGE_CODE == 'pl') {
-    $id = 2786;
+  $id = 2786;
 } elseif (ICL_LANGUAGE_CODE == 'ru') {
-    $id = 2787;
+  $id = 2787;
 } else {
-    $id = 2784;
+  $id = 2784;
 };
 
 $delivery_conditions_title = get_field('delivery_conditions_title', $id);
@@ -47,220 +63,12 @@ $сurrency_type = get_field('сurrency_type', $id);
 $length_title = get_field('length_title', $id);
 $length_blocks = get_field('length_block', $id);
 
-$images_carbon = carbon_get_the_post_meta('code_catalog_photo');
-$code_catalog_time = carbon_get_the_post_meta('code_catalog_time');
-
-$subheadline = get_field('subheadline');
-$color_options = get_field('color_options');
-$production_time_header = get_field('production_time_header');
-$header_length_options = get_field('header_length_options');
-$length_options = get_field('length_options');
-$number_of_rooms_title = get_field('number_of_rooms_title');
-$number_of_rooms = get_field('number_of_rooms');
-$description = get_field('description');
-$form_top = get_field('form_top');
-$turnkey_configuration = get_field('turnkey_configuration');
-$turnkey_package_header = get_field('turnkey_package_header');
-$catId = $term_model[0]->term_taxonomy_id;
-
-$price = get_field('price');
-$characteristics = get_field('characteristics');
-$services = get_field('services');
-
-if ($term_model_item === 'postrojki-iz-brusa-ru') {
-    $images = get_field('gallery');
+if ($catId === 14 || $catId === 26 || $catId === 27 || $catId === 63) {
+  $showClassCalc = 'show-calc';
 } else {
-    $images = carbon_get_the_post_meta('code_catalog_photo');
+  $showClassCalc = '';
 }
-
-if ($catId === 14 || $catId === 26 || $catId === 27) {
-    $showClassCalc = 'show-calc';
-} else {
-    $showClassCalc = '';
-}
-
 ?>
-<section class="slider-info">
-    <div class="container">
-        <div class="slider-info__wrapper">
-            <h1 class="slider-mobile-title"><?= the_title() ?></h1>
-            <div class="slider-info__left">
-                <div class="swiper mySwiper2">
-                    <div class="swiper-wrapper">
-                        <?php if (!empty($images)) : ?>
-                            <?php foreach ($images as $image) {
-                                if ($term_model_item === 'postrojki-iz-brusa-ru') {
-                                    $image = $image['url'];
-                                } else {
-                                    $image = $image['code_catalog_photo_item'];
-                                }
-                                ?>
-                                <div class="swiper-slide">
-                                    <a href="javascript:;" data-fancybox="slider" data-src="<?= $image ?>"
-                                       class="slider-open-window">
-                                        <picture>
-                                            <img class="slide-main-image" src="<?= $image; ?>" alt="slide">
-                                        </picture>
-                                    </a>
-                                </div>
-                            <?php } ?>
-                        <?php endif; ?>
-                    </div>
-                    <div class="swiper-pagination"></div>
-                </div>
-                <div thumbsSlider="" class="swiper mySwiper">
-                    <div class="swiper-wrapper">
-                        <?php if (!empty($images)) : ?>
-                            <?php foreach ($images as $image) {
-                                if ($term_model_item === 'postrojki-iz-brusa-ru') {
-                                    $image = $image['url'];
-                                } else {
-                                    $image = $image['code_catalog_photo_item'];
-                                }
-                                ?>
-                                <div class="swiper-slide">
-                                    <img class="slide-main-image" src="<?= $image; ?>"
-                                         alt="slide">
-                                </div>
-                            <?php } ?>
-                        <?php endif; ?>
-                    </div>
-                </div>
-            </div>
-            <div class="slider-info__right">
-                <h1><?= the_title() ?></h1>
-                <?php if ($term_model_item === 'postrojki-iz-brusa-ru') : ?>
-                    <?php if (!empty($price)) : ?>
-                        <h2 class="slider-info__right-price"><span><?= $price ?></span> zł</h2>
-                    <?php endif; ?>
-
-                    <?php if (!empty($services)) : ?>
-                        <div class="slider-info__services">
-                            <h4>Возможные услуги</h4>
-                            <?php foreach ($services as $service) : ?>
-                                <?php if ($service['service_name']
-                                    && $service['service_price']) : ?>
-                                    <div class="service-item">
-                                        <div class="service-item-name"><?= $service['service_name'] ?></div>
-                                        <div class="service-item-switcher"
-                                             data-price="<?= $service['service_price'] ?>"><span></span></div>
-                                    </div>
-                                <?php endif; ?>
-                            <?php endforeach; ?>
-                        </div>
-                    <?php endif; ?>
-                <?php else: ?>
-                    <?php if (!empty($subheadline)) : ?>
-                        <div class="attr-subheadline"><?= $subheadline ?></div>
-                    <?php endif; ?>
-                    <?php if (!empty($color_options['main_colors'])) : ?>
-                        <div class="attr-colors-wrapper">
-                            <?php if (!empty($color_options['primary_colors_header'])) : ?>
-                                <p class="attr-colors-headline"><?= $color_options['primary_colors_header'] ?></p>
-                            <?php endif; ?>
-                            <div class="attr-colors">
-                                <?php foreach ($color_options['main_colors'] as $color_main) : ?>
-                                    <div class="attr-color" style="background-color: <?= $color_main['color'] ?>"></div>
-                                <?php endforeach; ?>
-                            </div>
-                        </div>
-                    <?php endif; ?>
-                    <?php if (!empty($code_catalog_time)) : ?>
-                        <div class="attr-production_time">
-                            <p class="attr-production_time-headline"><?= $production_time_header ?></p>
-                            <p class="attr-production_time-timing"><?= $code_catalog_time ?></p>
-                        </div>
-                    <?php endif; ?>
-                    <?php if (!empty($length_options)) : ?>
-                        <div class="attr-length_options-wrapper">
-                            <?php if (!empty($header_length_options)) : ?>
-                                <p class="attr-length_options-headline"><?= $header_length_options ?></p>
-                            <?php endif; ?>
-                            <div class="attr-length_options">
-                                <?php foreach ($length_options as $length_option) : ?>
-                                    <div class="attr-length_option"><?= $length_option['length'] ?></div>
-                                <?php endforeach; ?>
-                            </div>
-                        </div>
-                    <?php endif; ?>
-                    <?php if (!empty($number_of_rooms)) : ?>
-                        <div class="attr-rooms">
-                            <p class="attr-rooms-headline"><?= $number_of_rooms_title ?></p>
-                            <p class="attr-rooms-timing"><?= $number_of_rooms ?></p>
-                        </div>
-                    <?php endif; ?>
-                    <?php if ($turnkey_configuration) : ?>
-                        <div class="attr-configuration">
-                            <p class="attr-configuration-headline"><?= $turnkey_package_header ?></p>
-                            <div class="attr-configuration-svg">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50"
-                                     fill="none">
-                                    <path d="M25 48.4375C37.9442 48.4375 48.4375 37.9442 48.4375 25C48.4375 12.0558 37.9442 1.5625 25 1.5625C12.0558 1.5625 1.5625 12.0558 1.5625 25C1.5625 37.9442 12.0558 48.4375 25 48.4375Z"
-                                          fill="#4BD37B"/>
-                                    <path d="M35.9375 10.9375L19.5312 27.8125L14.0625 22.1875L8.59375 27.8125L19.5312 39.0625L41.4062 16.5625L35.9375 10.9375Z"
-                                          fill="white"/>
-                                </svg>
-                            </div>
-                        </div>
-                    <?php endif; ?>
-                    <?php if (!empty($color_options['roof_colors'])) : ?>
-                        <div class="attr-colors-wrapper">
-                            <?php if (!empty($color_options['roof_colors_title'])) : ?>
-                                <p class="attr-colors-headline"><?= $color_options['roof_colors_title'] ?></p>
-                            <?php endif; ?>
-                            <div class="attr-colors">
-                                <?php foreach ($color_options['roof_colors'] as $color_roof) : ?>
-                                    <div class="attr-color" style="background-color: <?= $color_roof['color'] ?>"></div>
-                                <?php endforeach; ?>
-                            </div>
-                        </div>
-                    <?php endif; ?>
-                    <?php if (!empty($form_top)) : ?>
-                        <div class="attr-form"><?= $form_top ?></div>
-                    <?php endif; ?>
-                <?php endif; ?>
-            </div>
-        </div>
-        <?php if ($term_model_item === 'postrojki-iz-brusa-ru') : ?>
-            <div class="slider-info-tabs">
-                <?php if (!empty($characteristics)) : ?>
-                    <h4 class="slider-info-tab-characteristics js-active-item" data-class="slider-info-characteristics">
-                        Характеристики</h4>
-                <?php endif; ?>
-                <?php if (!empty($description)) : ?>
-                    <h4 class="slider-info-tab-description" data-class="slider-info-description">Описание</h4>
-                <?php endif; ?>
-            </div>
-            <div class="slider-info-content">
-                <?php if (!empty($characteristics)) : ?>
-                    <div class="slider-info-characteristics js-active-item">
-                        <?php foreach ($characteristics as $characteristic) : ?>
-                            <div class="characteristic-item">
-                                <?php if (!empty($characteristic)) : ?>
-                                    <div class="description-characteristic-name"><?= $characteristic['characteristic_name'] ?>
-                                        :
-                                    </div>
-                                <?php endif; ?>
-                                <?php if (!empty($characteristic)) : ?>
-                                    <div class="description-characteristic-value"><?= $characteristic['characteristic_value'] ?></div>
-                                <?php endif; ?>
-                            </div>
-                        <?php endforeach; ?>
-                    </div>
-                <?php endif; ?>
-                <?php if (!empty($description)) : ?>
-                    <div class="slider-info-description"><?= $description ?></div>
-                <?php endif; ?>
-            </div>
-        <?php else: ?>
-            <?php if (!empty($description)) : ?>
-                <div class="attr-description"><?= $description ?></div>
-            <?php endif; ?>
-
-        <?php endif; ?>
-    </div>
-</section>
-
 
 <section class="calculator <?= $showClassCalc ?>">
     <h2 style="text-align: center;">
@@ -706,7 +514,7 @@ if ($catId === 14 || $catId === 26 || $catId === 27) {
         </div>
     </div>
 </section>
+
 <?php
 get_footer();
 ?>
-
