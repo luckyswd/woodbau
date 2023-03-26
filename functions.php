c1c5369b94baf83d0b4fa2765f6939866777d3c1
denis.shediakov <luckyswd@gmail.com>
2023-03-26 20:10:21 +0300

    deploy

--- functions.php
+++ functions.php
@@ -308,7 +308,7 @@ function wood_scripts_styles()
         $fileName = explode('/', $filePath);
         $fileName = end($fileName);
         $fileName = str_replace('.css', ' ', $fileName);
-        wp_register_style(trim($fileName),get_template_directory_uri() . $filePath, array(), '1.2', 'screen');
+        wp_register_style(trim($fileName),get_template_directory_uri() . $filePath, array(), getRandomVersion(), 'screen');
         wp_enqueue_style(trim($fileName));
     }
 
@@ -317,7 +317,7 @@ function wood_scripts_styles()
         $fileName = explode('/', $filePath);
         $fileName = end($fileName);
         $fileName = str_replace('.js', ' ', $fileName);
-        wp_enqueue_script(trim($fileName),get_template_directory_uri() . $filePath, array(), '1.0', true);
+        wp_enqueue_script(trim($fileName),get_template_directory_uri() . $filePath, array(), getRandomVersion(), true);
     }
 
     wp_enqueue_script('masonry', get_template_directory_uri() . '/js/masonry.pkgd.min.js', array(), '1.0', true);
@@ -392,4 +392,9 @@ if (function_exists('acf_add_options_page')) {
         'capability' => 'edit_posts',
         'redirect' => false
     ));
+}
+
+function getRandomVersion(): string
+{
+    return sprintf('1.%s.%s', rand(1, 99), rand(1, 99));
 }
