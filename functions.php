58c8d638c107addbfe17740fd269f973c752e3df
denis.shediakov <luckyswd@gmail.com>
2023-03-27 22:26:15 +0300

    add hash

--- functions.php
+++ functions.php
@@ -308,7 +308,7 @@ function wood_scripts_styles()
         $fileName = explode('/', $filePath);
         $fileName = end($fileName);
         $fileName = str_replace('.css', ' ', $fileName);
-        wp_register_style(trim($fileName),get_template_directory_uri() . $filePath, array(), getRandomVersion(), 'screen');
+        wp_register_style(trim($fileName),get_template_directory_uri() . $filePath, array(), getHasFile('css/style.min.css'), 'screen');
         wp_enqueue_style(trim($fileName));
     }
 
@@ -317,7 +317,7 @@ function wood_scripts_styles()
         $fileName = explode('/', $filePath);
         $fileName = end($fileName);
         $fileName = str_replace('.js', ' ', $fileName);
-        wp_enqueue_script(trim($fileName),get_template_directory_uri() . $filePath, array(), getRandomVersion(), true);
+        wp_enqueue_script(trim($fileName),get_template_directory_uri() . $filePath, array(), getHasFile('js/build-min.js'), true);
     }
 
     wp_enqueue_script('masonry', get_template_directory_uri() . '/js/masonry.pkgd.min.js', array(), '1.0', true);
@@ -394,7 +394,10 @@ if (function_exists('acf_add_options_page')) {
     ));
 }
 
-function getRandomVersion(): string
-{
-    return sprintf('1.%s.%s', rand(1, 99), rand(1, 99));
+function getHasFile(
+    string $path
+): string {
+    $fullPath = sprintf('%s/dist/%s', __DIR__, $path);
+
+    return hash_file('md5', $fullPath) ?? '';
 }
